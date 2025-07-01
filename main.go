06dd00b12d4acb4c19325158167cb5dee92b010f
Budhayan Chakraborty <budhayanc2005@gmail.com>
2025-07-01 10:29:21 +0530

    feat(packages): Identify all packages installed

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
 	"runtime"
 	"strings"
 )
@@ -21,9 +22,10 @@ func main() {
 		if(distro=="unknown" && base_distro=="unknown"){fmt.Println("Failed to fetch the details of your distro")}
 		fmt.Println("Distribution:", distro)
 		fmt.Println("Built On:", base_distro)
+		fmt.Println(fetchPackages(base_distro))
 	default:
 		fmt.Println("OS not supported")
-
+	
 	}
 }
 
@@ -45,3 +47,28 @@ func fetchLinusDistro() (string, string) {
 
 	return distro, base_distro
 }
+
+func fetchPackages(base_distro string) []string {
+	var cmd *exec.Cmd
+	switch(base_distro) {
+	case "debian":
+		cmd = exec.Command("dpkg", "--get-selections")
+	case "arch":
+		cmd = exec.Command("pacman", "-Q")
+	case "rhel", "fedora":
+		cmd = exec.Command("rpm", "-qa")
+	case "void":
+		cmd = exec.Command("xbps-query", "-l")
+	default :
+		fmt.Println("Your distro is unsupported, cannot identify package manager !")
+		return []string{"unknown"}
+	}
+
+	output, err := cmd.CombinedOutput()
+	if (err != nil) {
+		fmt.Println("Error in retrieving packages: ", err)
+	}
+	fmt.Println("Installed Packages: ")
+	packages:=strings.Split(string(output), "\n")
+	return packages
+}	
