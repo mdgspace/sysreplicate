e295edd6f385bd5097bab77674fdce6987db2fd5
Siddharth Chopra <contact.sid.chopra@gmail.com>
2025-07-09 03:33:43 +0530

    feat: add support for flatpak, snap. Fix debian native packages, also refactor previous packages code

--- system/output/json.go
+++ system/output/json.go
@@ -5,28 +5,15 @@ import (
 )
 
 // BuildSystemJSON creates a well-structured JSON object for the system info and packages.
-func BuildSystemJSON(osType, distro, baseDistro string, packages []string) ([]byte, error) {
-	type ArchPackages struct {
-		Official []string `json:"official_packages"`
-		AUR      []string `json:"aur_packages"`
-	}
+func BuildSystemJSON(osType, distro, baseDistro string, packages map[string][]string) ([]byte, error) {
+
 	type SystemInfo struct {
-		OS         string      `json:"os"`
-		Distro     string      `json:"distro"`
-		BaseDistro string      `json:"base_distro"`
-		Packages   interface{} `json:"packages"`
+		OS         string              `json:"os"`
+		Distro     string              `json:"distro"`
+		BaseDistro string              `json:"base_distro"`
+		Packages   map[string][]string `json:"packages"`
 	}
-	if baseDistro == "arch" {
-		official, aur := SplitArchPackages(packages)
-		archPkgs := ArchPackages{Official: official, AUR: aur}
-		info := SystemInfo{
-			OS:         osType,
-			Distro:     distro,
-			BaseDistro: baseDistro,
-			Packages:   archPkgs,
-		}
-		return json.MarshalIndent(info, "", "  ")
-	}
+
 	info := SystemInfo{
 		OS:         osType,
 		Distro:     distro,
